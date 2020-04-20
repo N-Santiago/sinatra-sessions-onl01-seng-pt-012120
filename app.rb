1996189c40de42b507ae21ab4f628c1f801f8516
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-20 22:24:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -51,7 +51,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    session.delete(:id)  
+    session.clear(:id) #code to delete the session  
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
